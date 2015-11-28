2376f04f61d401356ec7b76de49f1ffcc8b37442
Kalbintion <distantimage24@gmail.com>
2015-11-28 07:07:43 -0600

    Initial

---
+++ Main.js
@@ -0,0 +1,275 @@
+// ==UserScript==
+// @name         Gulf Stream Modifications
+// @namespace    https://gulfstream.fidlar.com
+// @version      0.9
+// @description  Modifies the Gulfstream website in various ways to provide a better user interface
+// @author       Kalbintion
+// @include		 https://gulfstream.fidlar.com/Views/GulfStream/GulfStream
+// @include		 https://gulfstream.fidlar.com/Views/GulfStream/GulfStream/
+// @grant        none
+// @icon         https://gulfstream.fidlar.com/Content/Images/favicon.ico
+// @icon64       https://gulfstream.fidlar.com/Content/Images/favicon.ico
+// ==/UserScript==
+
+
+// ==========================================================================================================
+// Settings
+// ==========================================================================================================
+// For Keycodes see http://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
+// Use -1 to disable
+var KEY_LAST_DOCUMENT = 37; // Goes to the previous document
+var KEY_NEXT_DOCUMENT = 39; // Goes to the next document
+var KEY_ADD_PAGE = 107; // Adds page to document
+var KEY_NEXT_DOCUMENT_FOCUS = -1; // Focuses input onto Document name textbox
+var KEY_PAGE_NUMBER_FOCUS = -1; // Focuses input onto page number textbox
+var KEY_RENAME_YES = -1; // "Yes" on the rename dialog
+var KEY_RENAME_NO = -1; // "No" on the rename dialog
+var KEY_MULTI_DOC = 106; // Brings up the multiple document window
+var KEY_BLANK_PAGE = 111; // Marks page as blank
+var KEY_UNDO = -1; // Undo button (black toolbar, <)
+var KEY_REDO = -1; // Redo button (black toolbar, >)
+var KEY_CCW_ROTATE = -1; // Counter-clockwise rotate (black toolbar, < circle)
+var KEY_CW_ROTATE = -1; // Clockwise rotate (black toolbar, > circle)
+
+// Module enable/disable
+var allowImageLocModification = true; // This module tries to modify the image on the screen to its original
+var allowTimer = true; // This module adds a timer to the upper left of the page area indicating log in time
+
+
+// ==========================================================================================================
+// Key Shortcuts
+// ==========================================================================================================
+document.body.addEventListener("keydown", keyDownTextField, false);
+function keyDownTextField(e) {
+  var code = e.keyCode;
+  switch ( code )
+  {
+    case KEY_LAST_DOCUMENT:
+      document.getElementById("MainContent_DefaultMainContent_lbLastDocument").click();
+      break;
+    case KEY_NEXT_DOCUMENT:
+      document.getElementById("MainContent_DefaultMainContent_lbNextDocument").click();
+      break;
+    case KEY_ADD_PAGE:
+      document.getElementById("MainContent_DefaultMainContent_RadButtonAddPage").click();
+      break;
+    case KEY_NEXT_DOCUMENT_FOCUS:
+      document.getElementById("ctl00_ctl00_MainContent_DefaultMainContent_TextBoxNextDocument").focus();
+      break;
+    case KEY_PAGE_NUMBER_FOCUS:
+      document.getElementById("MainContent_DefaultMainContent_CurrentPageNumber").focus();
+      break;
+    case KEY_RENAME_YES:
+      document.getElementById("ctl00_ctl00_MainContent_DefaultMainContent_RadWindowRenameDocument_C_RadButtonYes").click();
+      break;
+    case KEY_RENAME_NO:
+      document.getElementById("ctl00_ctl00_MainContent_DefaultMainContent_RadWindowRenameDocument_C_RadButtonNo").click();
+      break;
+    case KEY_MULTI_DOC:
+      document.getElementById("ctl00_ctl00_MainContent_DefaultMainContent_RadComboBoxSpecialCondition_Arrow").click();document.getElementById("ctl00_ctl00_MainContent_DefaultMainContent_RadComboBoxSpecialCondition_DropDown").lastChild.lastChild.lastChild.click();
+      break;
+    case KEY_BLANK_PAGE:
+      document.getElementById("ctl00_ctl00_MainContent_DefaultMainContent_RadComboBoxSpecialCondition_Arrow").click();document.getElementById("ctl00_ctl00_MainContent_DefaultMainContent_RadComboBoxSpecialCondition_DropDown").lastChild.lastChild.children[2].click();
+      break;
+    case KEY_UNDO:
+      document.getElementsByClassName("darkroom-toolbar-actions")[0].children[0].children[0].click();
+      break;
+    case KEY_REDO:
+      document.getElementsByClassName("darkroom-toolbar-actions")[0].children[0].children[1].click();
+      break;
+    case KEY_CCW_ROTATE:
+      document.getElementsByClassName("darkroom-toolbar-actions")[0].children[1].children[0].click();
+      break;
+    case KEY_CW_ROTATE:
+      document.getElementsByClassName("darkroom-toolbar-actions")[0].children[1].children[1].click();
+      break;
+    default:
+      break;
+  }
+}
+
+// ==========================================================================================================
+// Modifies the image link to show original image
+// ==========================================================================================================
+var imageLinkTimer;
+if (allowImageLocModification) {
+    imageLinkTimer = setInterval(ModifyImageLink, 250);
+}
+
+function ModifyImageLink() {
+    var obj = document.getElementById("documentImage");
+    if(obj === null) {
+        clearInterval(imageLinkTimer);
+        return;
+    } else if(obj.src.indexOf("index") != -1) {
+        obj.src = obj.src.replace("index?", "OriginalImage?");
+        obj.style.display = "";
+        obj.style.width = "1150px";
+    }
+}
+
+// ==========================================================================================================
+// Multidoc Window Button Position
+// ==========================================================================================================
+var multidocTimer = setInterval(ModifyMultidoc, 250);
+function ModifyMultidoc() {
+   var obj = document.getElementById("RadWindowWrapper_ctl00_ctl00_MainContent_DefaultMainContent_RadWindowMultipleImages");
+   if(obj === null) {
+       // clearInterval(multidocTimer);
+       return;
+   } else {
+       var parentGroup = document.getElementById("ctl00_ctl00_MainContent_DefaultMainContent_RadWindowMultipleImages_C").children[0];
+       var buttonGroup = document.getElementById("ctl00_ctl00_MainContent_DefaultMainContent_RadWindowMultipleImages_C").children[0].lastElementChild;
+       if(~buttonGroup.innerHTML.indexOf("button")) {
+           parentGroup.insertBefore(buttonGroup, document.getElementById("ctl00_ctl00_MainContent_DefaultMainContent_RadWindowMultipleImages_C").children[0].children[0]);
+       }
+   }
+}
+
+// ==========================================================================================================
+// Time Tracking System
+// ==========================================================================================================
+// Login Timer Div
+var divLoginTimer = document.createElement("div");
+divLoginTimer.id = "loginTimer_div";
+divLoginTimer.style.color = "#FFF";
+divLoginTimer.style.position = "absolute";
+divLoginTimer.style.left = "25px";
+divLoginTimer.style.top = document.getElementsByTagName("nav")[0].offsetHeight + 22 + "px";
+
+// Login Timer Input
+var inputLoginTimer = document.createElement("input");
+inputLoginTimer.id = "loginTimer";
+inputLoginTimer.disabled = "disabled";
+inputLoginTimer.value = "0h 0m 0s";
+inputLoginTimer.style.border = "1px solid black";
+inputLoginTimer.style.color = "#FFF";
+inputLoginTimer.style.backgroundColor = "#000";
+inputLoginTimer.style.padding = "1px 5px";
+inputLoginTimer.style.width = "100px";
+divLoginTimer.innerHTML = "Timer: ";
+divLoginTimer.appendChild(inputLoginTimer);
+
+// div location reset on resize
+window.addEventListener("resize", function(e) {
+    divLoginTimer.style.top = document.getElementsByTagName("nav")[0].offsetHeight + 22 + "px";
+});
+
+// Reset Button
+var inputResetButton = document.createElement("input");
+inputResetButton.id = "loginTimerReset";
+inputResetButton.type = "button";
+inputResetButton.onclick=timeReset;
+inputResetButton.value="Reset";
+inputResetButton.style.border = "1px solid black";
+inputResetButton.style.backgroundColor = "#000";
+inputResetButton.style.padding = "1px 5px";
+divLoginTimer.appendChild(document.createElement("br"));
+divLoginTimer.appendChild(inputResetButton);
+
+// Append the timer to the page
+document.body.appendChild(divLoginTimer);
+// onbeforeunload Event
+window.addEventListener("beforeunload", function(e){
+   setCookie("unloadTime", Date());
+}, false);
+
+// Initiate the tracker
+timeTrackerInit();
+
+function timeTrackerInit() {
+   if(getCookieValue("startTime") !== null) {
+      // We have a startTime from before - do the difference on the dates and push it to logged time
+      var startDate = new Date(getCookieValue("startTime"));
+      var tickDate = new Date(getCookieValue("tickTime"));
+      setCookie("time", Number(getCookieValue("time")) + getTimeDifference(startDate, tickDate));
+   }
+    
+   // Page Load Check (Continuation)
+   if(getCookieValue("unloadTime") !== null && getCookieValue("unloadTime") !== "") {
+       var pageLoadTime = getTimeDifference(new Date(getCookieValue("unloadTime")), Date());
+       console.log("pageLoadTime: " + pageLoadTime);
+       if(pageLoadTime <= 10000) { // Only triggers if it has been less than 10 seconds
+           setCookie("time", Number(getCookieValue("time")) + pageLoadTime);
+           setCookie("unloadTime", null);
+       }
+   }
+   
+   // Reset both tickTime and startTime to now after calculations have been completed
+   setCookie("startTime", Date());
+   setCookie("tickTime", Date());
+    
+   // Initiate the timer
+   setInterval(timeUpdate, 1000, inputLoginTimer);
+    
+   // Update display immediately
+   timeUpdateDisplay(inputLoginTimer);
+}
+
+function timeUpdateDisplay(obj) {
+   // Existing + Unaccounted marked time
+   var milliseconds = (Number(getCookieValue("time")) + Number(getTimeDifference(getCookie("startTime"), getCookie("tickTime")))) / 1000;
+   var sec = Math.floor(milliseconds) % 60;
+   var min = Math.floor(milliseconds / 60) % 60;
+   var hr = Math.floor(milliseconds / 60 / 60);
+    
+   obj.value = hr + "h " + min + "m " + sec + "s";
+   obj.title = milliseconds + "s";
+}
+
+function timeUpdate(obj) {
+   setCookie("tickTime", Date());
+   timeUpdateDisplay(obj);
+}
+
+function getTimeDifference(date1, date2) {
+   return new Date(date2).getTime() - new Date(date1).getTime();   
+}
+
+function timeReset() {
+    var res = confirm("Do you wish to reset the time?");
+    if(res === true) {
+        setCookie("time", "0");
+        setCookie("startTime", Date());
+        setCookie("tickTime", Date());
+    }
+}
+
+// Keeps track of time spent logged in
+/* setInterval(addSecond, 1000, inputLoginTimer);
+
+function addSecond(obj) {
+    time+=1;
+    setCookie("time", time);
+    var sec = time % 60;
+    var min = Math.floor(time / 60) % 60;
+    var hr = Math.floor(time / 60 / 60);
+    obj.value = hr + "h " + min + "m " + sec + "s";
+    obj.title = time + "s";
+}
+
+function resetTime() {
+    var res = confirm("Do you wish to reset the time?");
+    if(res === true) {
+        time=0;
+        setCookie("time", time);
+    }
+} */
+
+// ==========================================================================================================
+// Cookie Functions
+function getCookie(name) { return getCookieValue(name); }
+
+function getCookieValue(name) {
+    var keyValue = document.cookie.split("; ");
+    for (i = 0; i < keyValue.length; i++) {
+        var pairs = keyValue[i].split("=");
+        if(pairs[0] == name)
+            return pairs[1];
+    }
+    return null;
+}
+
+function setCookie(name, value) {
+    document.cookie=name + "=" + value;
+}
---
+++ Productivity.js
@@ -0,0 +1,73 @@
+// ==UserScript==
+// @name         Gulf Stream Modifications - Productivity
+// @namespace    https://gulfstream.fidlar.com
+// @version      0.2
+// @description  
+// @author       Kalbintion
+// @include		 https://gulfstream.fidlar.com/Account/StreamerInformation
+// @include		 https://gulfstream.fidlar.com/Account/StreamerInformation/
+// @grant        none
+// @icon         https://gulfstream.fidlar.com/Content/Images/favicon.ico
+// @icon64       https://gulfstream.fidlar.com/Content/Images/favicon.ico
+// ==/UserScript==
+
+// Settings
+var docValue = 0.026422
+var multValue = 0.015
+var cropValue = 0.012
+
+// Elements
+var holder = document.getElementsByClassName("col-md-6")[0].children[0];
+holder.appendChild(createNewEntry("documentCnt", "Document $"));
+holder.appendChild(createNewEntry("multidocCnt", "MultiDoc $"));
+holder.appendChild(createNewEntry("crop_docCnt", "Crop Doc $"));
+holder.appendChild(createNewEntry("totalAmt", "Total $"));
+updateMoneyAmounts();
+
+function updateMoneyAmounts() {
+    var docCnt = document.getElementsByTagName("input")[0].value;
+    var mulCnt = document.getElementsByTagName("input")[1].value;
+    var crpCnt = document.getElementsByTagName("input")[2].value;
+    
+    var docAmt = document.getElementById("documentCnt-divInput");
+    var mulAmt = document.getElementById("multidocCnt-divInput");
+    var crpAmt = document.getElementById("crop_docCnt-divInput");
+    var ttlAmt = document.getElementById("totalAmt-divInput");
+    
+    docAmt.value = roundToDecimal(docCnt * docValue, 2);
+    mulAmt.value = roundToDecimal(mulCnt * multValue, 2);
+    crpAmt.value = roundToDecimal(crpCnt * cropValue, 2);
+    ttlAmt.value = roundToDecimal(Number(docCnt * docValue) + Number(mulCnt * multValue) + Number(crpCnt * cropValue), 2);
+}
+
+function createNewEntry(name, label) {
+    var divHolder = document.createElement("div");
+    divHolder.className = "form-group";
+    divHolder.id = name + "-divHolder";
+   
+    var divLabel = document.createElement("label");
+    divLabel.className = "col-md-4 control-label";
+    divLabel.id = name + "-divLabel";
+    divLabel.innerHTML = label;
+   
+    var divInputHolder = document.createElement("div");
+    divInputHolder.className = "col-md-8";
+    divInputHolder.id = name + "-divInputHolder";
+    
+    var divInput = document.createElement("input");
+    divInput.className = "form-control";
+    divInput.id = name + "-divInput";
+    divInput.readOnly = true;
+    
+    divHolder.appendChild(divLabel);
+    divInputHolder.appendChild(divInput);
+    divHolder.appendChild(divInputHolder);
+    
+    return divHolder;
+}
+
+function roundToDecimal(value, places) {
+  value = value * Math.pow(10, places);
+  value = Math.round(value);
+  return value / Math.pow(10, places);
+}
